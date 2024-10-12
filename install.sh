ac183a7c06ccb0b493a16a1d15d53f69bd1a8873
Venkatesh P R <rcvenky@yahoo.com>
2024-10-12 16:25:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Downloading..."
-curl -L  https://github.com/gholker/led-backlight-cmstorm/blob/master/led-backlight-cmstorm?raw=true > /Applications/led-backlight-cmstorm
+curl -L  https://github.com/gholker/led-backlight-cmstorm/blob/master/led-backlight-cmstorm > /Applications/led-backlight-cmstorm
 
 echo "Making executable"
 chmod +x /Applications/led-backlight-cmstorm
